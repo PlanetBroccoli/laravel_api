d8ae417c84d057a03724701f2cc3977bfbd9d0b6
Jocy <jocy@Jocyde-MacBook-Pro.local>
2018-10-20 18:08:47 +0800

    debug

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 's3php'),
+    'default' => env('FILESYSTEM_DRIVER', 's3'),
 
     /*
     |--------------------------------------------------------------------------

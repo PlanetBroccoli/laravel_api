d8b6312b4fccb038c5a5f1583ba7264cd3c6540c
Jocy <jocy@Jocyde-MacBook-Pro.local>
2018-10-21 01:16:15 +0800

    debug type

--- app/Report.php
+++ app/Report.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     //
+    protected $fillable = [
+        'userId','file', 'latitude', 'longitude', 'type', 'description'
+    ];
 }
